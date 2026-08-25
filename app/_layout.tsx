935393aee0d1067d6be251bd4fa1e141943465c9
Naveed Qadir <naveedqadir0@gmail.com>
2026-08-25 13:25:57 +0530

    Init

---
+++ app/_layout.tsx
@@ -0,0 +1,71 @@
+import { Fab, FabIcon } from '@/components/ui/fab';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import { MoonIcon, SunIcon } from '@/components/ui/icon';
+import '@/global.css';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from 'expo-router';
+import { useFonts } from 'expo-font';
+import { Slot, usePathname } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const pathname = usePathname();
+  const [colorMode, setColorMode] = useState<'light' | 'dark' | 'system'>(
+    'dark'
+  );
+
+  return (
+    <ThemeProvider value={colorMode === 'dark' ? DarkTheme : DefaultTheme}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <GluestackUIProvider mode={colorMode}>
+          <StatusBar style={colorMode === 'dark' ? 'light' : 'dark'} />
+          <Slot />
+          {pathname === '/' && (
+            <Fab
+              onPress={() =>
+                setColorMode(colorMode === 'dark' ? 'light' : 'dark')
+              }
+              className="m-6"
+              size="lg"
+            >
+              <FabIcon as={colorMode === 'dark' ? MoonIcon : SunIcon} />
+            </Fab>
+          )}
+        </GluestackUIProvider>
+      </GestureHandlerRootView>
+    </ThemeProvider>
+  );
+}
